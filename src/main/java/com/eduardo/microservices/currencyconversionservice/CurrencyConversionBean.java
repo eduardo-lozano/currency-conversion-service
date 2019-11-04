91a9b4ac9d49848cfd2191bc56abb28f7664af87
Ed <ed.lozano.developer@gmail.com>
2019-11-04 11:55:27 -0500

    Implementing microservice - currency-converter-service - inside this microservice call the other one called currency-exchange.

---
+++ src/main/java/com/eduardo/microservices/currencyconversionservice/CurrencyConversionBean.java
@@ -0,0 +1,88 @@
+package com.eduardo.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionBean {
+
+	private long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionFactor;
+	private BigDecimal quantity;
+	private BigDecimal totalCalculatedAmount;
+	private int port;
+
+
+	// Default no-args constructor is required for JPA to be able to use this bean
+	public CurrencyConversionBean() {
+
+	}
+
+	public CurrencyConversionBean(long id, String from, String to, BigDecimal conversionFactor, BigDecimal quantity,
+			BigDecimal totalCalculatedAmount, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.quantity = quantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.port = port;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+
+	public void setConversionFactor(BigDecimal conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
+
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+}
---
+++ src/main/java/com/eduardo/microservices/currencyconversionservice/CurrencyConversionResource.java
@@ -0,0 +1,52 @@
+package com.eduardo.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionResource {
+	
+	@GetMapping("/currency-converter/from/{currencyFrom}/to/{currencyTo}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency(
+			@PathVariable String currencyFrom, @PathVariable String currencyTo, @PathVariable BigDecimal quantity) {
+		
+		/* Call the other microservice currency-exchange to get the exchange rate (a.k.a. conversionFactor) */
+		// Step 1) Create a Map<String, String> with the variable values we'll pass to the other microservice
+		Map<String, String> currenciesToConvert= new HashMap<String, String>();
+		currenciesToConvert.put("fromCurrency", currencyFrom);
+		currenciesToConvert.put("toCurrency", currencyTo);
+		
+		// Step 2) Use the method RestTemplate().getForEntity() passing the URL of the other microservice,
+		//         the expected return bean's class name, and the map from Step 1.
+		//         Note the variable names in the URI below is the same as the variable names in the Map above.
+		
+		ResponseEntity<CurrencyConversionBean> theOtherMicroservicesResponse = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{fromCurrency}/to/{toCurrency}",
+				CurrencyConversionBean.class, currenciesToConvert);
+		// Note that both beans (the one from the other microservice CurrencyExchangeValue.java and this one's
+		// CurrencyConversionBean.java) have almost the same field names and field types in common, so I assume
+		// it automatically fills the values from the other bean into the new local bean, for the common fields.
+		
+		// Step 3) Take the info from the received bean using method .getBody()
+		CurrencyConversionBean receivedBean = theOtherMicroservicesResponse.getBody();
+		BigDecimal conversionFactor =	receivedBean.getConversionFactor();
+
+		// Step 4) Use that info in this microservice
+		//         In this case, the receivedBean was created and automatically filled up with some info from the
+		//         other microservice, but some of its fields were not filled up. We'll have to fill them manually
+		//         before returning it.
+		receivedBean.setQuantity(quantity);
+		// Multiply the quantity by the conversion factor, to get the total
+		receivedBean.setTotalCalculatedAmount(quantity.multiply(conversionFactor));
+		// Note: For this case, the port number remains the one from the other microservice, we don't change it.
+						   
+		return receivedBean;
+	}
+}
